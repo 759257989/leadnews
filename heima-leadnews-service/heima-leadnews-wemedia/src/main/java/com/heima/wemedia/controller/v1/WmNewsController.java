bb24e60f7959f3c4fe13f09c53405795743bdd2e
759257989 <wang1999yu@gmail.com>
2025-01-19 12:10:58 -0800

    media side articles creation, articles upload for content check

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmNewsController.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmNewsController.java
@@ -1,7 +1,9 @@
 package com.heima.wemedia.controller.v1;
 
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.wemedia.dtos.WmNewsDto;
 import com.heima.model.wemedia.dtos.WmNewsPageReqDto;
+import com.heima.model.wemedia.pojos.WmNews;
 import com.heima.wemedia.service.WmNewsService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +20,15 @@ public class WmNewsController {
     @Autowired
     private WmNewsService wmNewsService;
 
+    //获取文章列表
     @PostMapping("/list")
     public ResponseResult findList(@RequestBody WmNewsPageReqDto dto) {
         return wmNewsService.findList(dto);
+    }
+
+    //发布文章
+    @PostMapping("/submit")
+    public ResponseResult submitNews(@RequestBody WmNewsDto dto) {
+        return wmNewsService.submitNews(dto);
     }
 }
--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/WmNewsService.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/WmNewsService.java
@@ -3,6 +3,7 @@ package com.heima.wemedia.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.wemedia.dtos.WmNewsDto;
 import com.heima.model.wemedia.dtos.WmNewsPageReqDto;
 import com.heima.model.wemedia.pojos.WmNews;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,4 +17,10 @@ public interface WmNewsService extends IService<WmNews> {
      */
     public ResponseResult findList( WmNewsPageReqDto dto);
 
+    /**
+     * 发布修改或保存为草稿
+     * @param dto
+     * @return
+     */
+    public ResponseResult submitNews(WmNewsDto dto);
 }
