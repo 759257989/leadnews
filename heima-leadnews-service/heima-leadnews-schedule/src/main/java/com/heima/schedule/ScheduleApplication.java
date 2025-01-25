97bdcdf2662647703ae58b8622576387503687f0
759257989 <wang1999yu@gmail.com>
2025-01-25 15:05:11 -0800

    delay queue module implementation

---
+++ heima-leadnews-service/heima-leadnews-schedule/src/main/java/com/heima/schedule/ScheduleApplication.java
@@ -0,0 +1,26 @@
+package com.heima.schedule;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@MapperScan("com.heima.schedule.mapper")
+public class ScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleApplication.class,args);
+    }
+
+    /**
+     * mybatis-plus乐观锁支持
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor optimisticLockerInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return interceptor;
+    }
+}
